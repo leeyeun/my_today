c0e76fdc5121a0611cd364b73acaaa4f8fbae781
leeyeun <dpdms0109@naver.com>
2022-12-16 00:58:42 +0900

    :sparkles: Feat : 오늘의 날씨

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import "./Main.scss";
 
 function Main() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  const nowDate = `${year}/${month}/${date}`;
   return (
     <div className="mainWrap">
-      <span>안녕하세요!</span>
+      <p className="todayDate">{nowDate}</p>
     </div>
   );
 }
